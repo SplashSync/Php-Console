72b0a59ffcfffbd1f2ba00174dcadccad690d0c4
BadPixxel <eshop.bpaquier@gmail.com>
2019-09-09 21:11:18 +0200

    WIP: Documentation Builder

--- docs/fr/assets/js/main.js
+++ docs/fr/assets/js/main.js
@@ -11,15 +11,34 @@ $(document).ready(function() {
     $('body').scrollspy({target: '#doc-menu', offset: 100});
     
     /* Smooth scrolling */
-	$('a.scrollto').on('click', function(e){
+    $('a.scrollto').on('click', function(e){
         var page = $(this).attr('href');    // Page cible
         var speed = 800;                    // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-	});
+    });
      
+    //------------------------------------------------------------------------------
+    // Detect Doc Images
+    var collection = $(".lightbox-content img");
+    if(collection.length) {
+        //------------------------------------------------------------------------------
+        // Init All Typed Texts
+        collection.each(function(){
+            // Extract Image Url
+            imgUrl = $( this ).attr('src');
+            // Build Lightvox Version
+            newHtml = '<div class="screenshot-holder">';
+            newHtml+= '<a href="' + imgUrl + '" data-toggle="lightbox"><img class="img-fluid" src="' + imgUrl + '" /></a>';
+            newHtml+= '<a class="mask" href="' + imgUrl + '" data-toggle="lightbox"><i class="icon fa fa-search-plus"></i></a>';
+            newHtml+= '</div>';
+            // Replace Image By Lightbox Version
+            $( this ).replaceWith(newHtml);
+                
+        });
+    }
+    
     /* Bootstrap lightbox */
     /* Ref: http://ashleydw.github.io/lightbox/ */
-
     $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
         e.preventDefault();
         $(this).ekkoLightbox();
---
+++ docs/reboot.sh
@@ -0,0 +1,6 @@
+cd .. 
+php vendor/bin/grumphp run --testsuite=splash
+cd .gh-pages
+bundle exec jekyll serve
+cd ..
+
